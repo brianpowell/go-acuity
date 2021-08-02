5710323aa5af0695b993635e7e04d82cdac7c196
Brian Powell <brian.powell@completeset.com>
2021-08-02 17:51:19 -0400

    remove paid from ApptBody model

--- models.go
+++ models.go
@@ -57,7 +57,6 @@ type (
 		LastName    string      `json:"lastName,omitempty"`
 		Email       string      `json:"email,omitempty"`
 		Phone       string      `json:"phone,omitempty"`
-		Paid        string      `json:"paid,omitempty"`
 		Timezone    string      `json:"timezone,omitempty"`
 		Certificate string      `json:"certificate,omitempty"`
 		Fields      []FieldPost `json:"fields,omitempty"`
